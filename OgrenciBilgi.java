4f8cda29daabac3c0d3a0a3ac6252bb2c46d32e7
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-24 15:19:03 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -17,17 +17,17 @@ public class odev1 {
 		System.out.println("=============================");
 		System.out.println("   Öğrenci Bilgi Sistemi");
 		
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);		//Kullanıcıdan girdi alan kod
 		System.out.println("Adınızı Girin : ");
-		String isim = scanner.nextLine();
+		String isim = scanner.nextLine();				//Kullanıcıdan ad girdisini alan kod
 		System.out.println("Soyadınızı Girin :");
-		String soyisim = scanner.nextLine();
+		String soyisim = scanner.nextLine();			//Kullanıcıdan soyad girdisini alan kod
 		System.out.println("Öğrenci Numaranızı Girin : ");
-		int ogr_no = scanner.nextInt();
+		int ogr_no = scanner.nextInt();					//Kullanıcıdan öğrenci numarası girdisini alan kod
 		System.out.println("Yaşınızı Girin : ");
-		int yas = scanner.nextInt();
+		int yas = scanner.nextInt();					//Kullanıcıdan yaş girdisini alan kod
 		System.out.println("GPA'nızı Girin : ");
-		double gpa = scanner.nextDouble();
+		double gpa = scanner.nextDouble();				//Kullanıcıdan gpa girdisini alan kod
 		
 		System.out.println("=============================");
 		System.out.println("   Öğrenci Bilgi Sistemi");
@@ -37,10 +37,10 @@ public class odev1 {
 		System.out.println("GPA : " + gpa);
 		
 		if (gpa > 2.0) {
-			System.out.println("Durum : Başarılı Öğrenci");
+			System.out.println("Durum : Başarılı Öğrenci");  	//Kullanıcının girdiği gpaya göre karar veren komut satırı
 		}
 		else {
-			System.out.println("Durum : Başarısız Öğrenci");
+			System.out.println("Durum : Başarısız Öğrenci");	//Kullanıcının girdiği gpaya göre karar veren komut satırı
 		}
 		
 	}
